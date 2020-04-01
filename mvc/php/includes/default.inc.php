ca1cc33cbaaad55c0c9f573faa80715ce264d50e
dylan <dylanspin100@hotmail.com>
2020-04-01 13:14:11 +0200

    start

--- mvc/php/includes/default.inc.php
+++ mvc/php/includes/default.inc.php
@@ -1,7 +1,7 @@
 <?php
 
 
-  define("WEB_ROOT", DIRECTORY_SEPARATOR . "php-4/test" . DIRECTORY_SEPARATOR);
+  define("WEB_ROOT", DIRECTORY_SEPARATOR . "php-4/mvc" . DIRECTORY_SEPARATOR);
   define("LOCAL_ROOT", $_SERVER['DOCUMENT_ROOT'] . WEB_ROOT);
 
   spl_autoload_register(function ($class_name) {
